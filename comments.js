e17c8ae80b62ccf75bc74c6b5077c0096079a2e5
Pratham Basnet <prathambasnet963@gmail.com>
2024-10-18 16:29:49 +0545

    learned about comments

---
+++ comments.js
@@ -0,0 +1,23 @@
+// Single line comment 
+
+// This is a single line comment
+
+// Multi line comment 
+
+/*
+Document or logic explain
+
+My
+name
+is 
+Pratham
+
+*/
+
+
+// Shortcut key -> ctrl + /
+
+// this 
+// is 
+// another
+// comment
