ddae19c2f9a3cd50548cc29dcc6f450ce4f60fec
Pratham Basnet <prathambasnet963@gmail.com>
2024-10-18 16:28:55 +0545

    learned about variables

---
+++ variables.js
@@ -0,0 +1,30 @@
+// ES5
+var person = "Pratham Basnet";
+
+// ES6
+const anotherPerson = "Amrit";
+let thirdPerson = "Ram";
+
+console.log(person, anotherPerson, thirdPerson);
+
+var test = "Pratham";
+var test = "sita";
+console.log(test);
+
+const testPerson = "Pratham"; // (fixed variables name declare)
+console.log(testPerson);
+
+let newPerson = "Pratham"; // (variable update garna dido raixa)
+newPerson = "Ramayan";
+
+console.log(newPerson);
+
+// Always start variable with const keyword
+const table = "red table";
+
+console.log(table);
+
+let phone = 9810646151;
+phone = 9704543535;
+
+console.log(phone); // 9704543535
